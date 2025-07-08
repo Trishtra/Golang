e0acf115e1575f959421fe960a9559093ad15016
Trishtra <antonustinnikov@bk.ru>
2025-07-08 16:29:51 +0300

    Update TZ.go

--- TZ.go
+++ TZ.go
@@ -1,5 +1,39 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
 
+func main() {
+	var guess int
+	rand.Seed(time.Now().UnixNano())
+	answer := rand.Intn(10) + 1
+
+	for count := 0; count < 5; count++ {
+
+		fmt.Println("Угадайте число от 1 до 10!")
+
+		_, err := fmt.Scan(&guess)
+		if err != nil {
+			fmt.Println("Вы ввели не число.")
+			return
+		}
+
+		if guess < answer {
+			fmt.Println("Загаданное число больше!")
+
+		} else if guess > answer {
+			fmt.Println("Загаданное число меньше!")
+
+		} else {
+			fmt.Printf("Вы угадали! Искомое число : %d", answer)
+			return
+		}
+
+		fmt.Printf("Осталось %d попыток\n", 4-count)
+	}
+
+	fmt.Printf("Вы проиграли! Искомое число : %d\n", answer)
 }
